05f32482c37e8a588dcfb5cab8d0b6db7aa87c8b
Drazen Minic <mdrazen27@gmail.com>
2024-04-28 12:37:47 +0200

    Create model for retrieving data from Alpha Vantage

--- README.md
+++ README.md
@@ -2,13 +2,15 @@
 
 ### App for tracking stock prices from Alpha Vantage API in real time
 
-
 Requirements
+
 ```
 "php": "^8.2"
 "MariaDB": : "10.4.32"
+ALPHA_VANTAGE_API_KEY in .env
 ```
 
+**API key can be retrieved from <a href="https://www.alphavantage.co/support/#api-key">Vantage alpha API key</a>*
 
 Commands to run
 
---
+++ app/Models/AlphaVantageApi.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+
+class AlphaVantageApi
+{
+    const BASE_URL = 'https://www.alphavantage.co/query';
+
+    public function getStockPrices(int $stockId, string $symbol, int $interval = 1, string $outputsize = 'compact', ?string $month = null): array|null
+    {
+        $response = Http::get(self::BASE_URL,
+            [
+                'function' => 'TIME_SERIES_INTRADAY',
+                'apikey' => env('ALPHA_VANTAGE_API_KEY'),
+                'interval' => $interval,
+                'symbol' => $symbol,
+                'outputsize' => $outputsize,
+                'month' => $month,
+            ]
+        );
+
+        $status = $response->status();
+        if ($status === 200) {
+            $dataObject = $response->object();
+            $metaData = $dataObject->{"Meta Data"} ?? null;
+            if ($metaData) {
+                $data = $dataObject->{"Time Series ($interval)"} ?? null;
+                $parsedValues = [];
+                foreach ($data as $dataKey => $dataValues) {
+                    $currentRow = ['time' => $dataKey, 'stock_id' => $stockId];
+                    foreach ($dataValues as $valueKey => $value) {
+                        // response returns open value as '1. open'...
+                        $currentRow[explode(' ', $valueKey)[1]] = $value;
+                    }
+                    $parsedValues[] = $currentRow;
+                }
+                return $parsedValues;
+            }
+        }
+        $this->logError($response);
+        return null;
+    }
+
+    private function logError($response): void
+    {
+        Log::error(json_encode(['body' => $response->body(), 'request_url' => $response->effectiveUri()]));
+    }
+}
